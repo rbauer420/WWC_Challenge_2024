e68b1eb1c6375ff8d4dc6ae85f6f8c47aa2f7e3a
rbauer420 <rachel.bauer1@gmail.com>
2024-01-10 16:01:51 -0500

    Project features and to do list

---
+++ WWC_Challenge_2024.r
@@ -0,0 +1,16 @@
+# Dashbboard features
+# Two columns: left will have inputs and right will display the data 
+# Inputs will allow the user to display data based on different variables such as social media preference
+### by platform, gender, parents income, etc.
+# Data vizualization will include graphs and graphics 
+# Export feature to export a report based on the user inputs
+
+# Analysis tasks: 
+# Create graphs showing distribution by mobile
+# Create graphs showing distribution by web
+# Create graphs showing distribution by gender
+# Create graphs showing distribution by university
+# Create variable to calculate total freqencies for mobile and web
+# Create variable to combine parents income into single variable
+# Create variable to combine parents age into single variable
+# Create filters to splice the data by: age, parents income, student loan debt, did not vote
